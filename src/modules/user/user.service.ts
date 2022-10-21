ce77b162f06197a6cf59dffab2f1c358eabf9c35
RosembergAraujo <rosembergv.filho@gmail.com>
2022-10-21 01:53:13 -0300

    User Controller

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/PrismaService';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { UpdateUserDto } from './dto/patch-user.dto';
 
 @Injectable()
 export class UserService {
